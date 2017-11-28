f3a9f5b89e8608fb713c007b12841b3fa3b31a90
timotius02 <timsitorus1@gmail.com>
2017-11-28 00:26:11 -0500

    adding front-end chenges to seperate branch

--- web/js/home.js
+++ web/js/home.js
@@ -1,20 +1,34 @@
-
 onLoaded(function() {
+  $(window)
+    .scroll(function() {
+      var top = $(document).scrollTop();
 
-	$(window).scroll(function() {
-		var top = $(document).scrollTop();
+      if (top > 150) $("header").removeClass("home");
+      else $("header").addClass("home");
+    })
+    .trigger("scroll");
 
-		if (top > 150)
-			$("header").removeClass("home")
-		else
-			$("header").addClass("home")
-	}).trigger("scroll")
+  $("header .logo").append($("<div>").addClass("trail t2"));
 
-	$("header .logo").append(
-		$("<div>").addClass("trail t2")
-		);
+  $(".overlapbox").on("click", function(e) {
+    e.preventDefault();
+    console.log(dateDepart.datepicker("getDate"));
+    console.log(dateReturn.datepicker("getDate"));
 
+    $.get("http://localhost:8080/Website/flights", function(data) {
+      console.log("success");
+      console.log(data);
+    });
+  });
 
-
-
+  $(".login").on("click", function(e) {
+    console.log("loggin in ");
+    $.post(
+      "http://localhost:8080/Website/login",
+      { username: "hello", password: "world" },
+      function(data) {
+        console.log(data);
+      }
+    );
+  });
 });
