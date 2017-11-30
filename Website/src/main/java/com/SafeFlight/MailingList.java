bdf372c1b603a96a43c8bb21a46634b9d561dcdf
Warren <wwong5415@gmail.com>
2017-11-30 01:24:35 -0500

    finished MailingList

---
+++ Website/src/main/java/com/SafeFlight/MailingList.java
@@ -0,0 +1,81 @@
+package com.SafeFlight;
+
+import java.io.IOException;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+/**
+ * Servlet implementation class MailingList
+ */
+public class MailingList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MailingList() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Servlet is called.");
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		JSONObject json = new JSONObject();
+		JSONArray jArray = new JSONArray();
+		
+		try {
+			Connection conn = ConnectionUtils.getMyConnection();
+			 System.out.println("Get connection " + conn);
+		        
+		        // Testing SQL
+		        Statement statement = conn.createStatement();
+		        String sql = "{CALL getMailingList()}";
+		        ResultSet rs = statement.executeQuery(sql);
+		        
+		        while(rs.next()) {
+		        		String email = rs.getString("email");
+		        		JSONObject o = new JSONObject();
+		        		o.put("Email", email);
+		        		jArray.add(o);
+		        }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        json.put("emails", jArray);
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().print(json);
+		response.getWriter().flush();
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
