9886e68a555841840aeca7285d2c838dd615b3cf
Roger <roger.li.1@stonybrook.edu>
2017-12-01 19:14:26 -0500

    .

---
+++ web/login.php
@@ -0,0 +1,52 @@
+<html>
+
+	<head>
+
+		<title>Login | SafeFlight</title>
+
+		<link rel=icon href=/img/favicon.ico>
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+		<script src="js/vendor/jmin.js"></script>
+		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+		<script src="js/script.js"></script>
+	</head>
+
+	<body>
+		
+    	<?php $headerHighlight = "Login"; include __DIR__."/common/header.php"; ?>
+
+    	<div id="container">
+    		<div id="content">
+
+    			<h1>Login</h1>
+
+                
+
+    		</div>
+    	</div>
+
+	</body>
+	
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
