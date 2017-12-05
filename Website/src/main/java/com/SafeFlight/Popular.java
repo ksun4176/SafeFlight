9b9bfbd0ac5d8a6339ee0d88f962bc98b66c21d3
Kevin Sun <kevin.sun.1@stonybrook.edu>
2017-12-05 16:15:52 -0500

    fixed getpopular to fit api

--- Website/src/main/java/com/SafeFlight/Popular.java
+++ Website/src/main/java/com/SafeFlight/Popular.java
@@ -1,10 +1,12 @@
 package com.SafeFlight;
 
 import java.io.IOException;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -35,27 +37,72 @@ public class Popular extends HttpServlet {
 		// TODO Auto-generated method stub
 		System.out.println("Servlet is called.");
 		JSONObject json = new JSONObject();
-		JSONArray flights = new JSONArray();
+		JSONArray jArray = new JSONArray();
 		
 		try {
 			Connection conn = ConnectionUtils.getMyConnection();
 
-		        // Testing SQL
-		        Statement statement = conn.createStatement();
-		        String sql = "{CALL getMostActiveFlights}";
-		        ResultSet rs = statement.executeQuery(sql);
-		        
+				ArrayList<String> flightNames = new ArrayList<String>();
+		        String query = "{CALL getMostActiveFlights()}";
+		        CallableStatement stmt = conn.prepareCall(query);
+		        ResultSet rs = stmt.executeQuery();
 		        while(rs.next()) {
-		        		String airlineId = rs.getString("AirlineId");
-		        		int flightNo = rs.getInt("FlightNo");
-		        		int Amount = rs.getInt("Amount");
-		        		
 		        		JSONObject o = new JSONObject();
-		        		o.put("airline_id", airlineId);
-		        		o.put("flightNumber", flightNo);
-		        		o.put("Amount", Amount);
-		        		flights.add(o);
+		        		String airline_id = rs.getString("AirlineId");
+		        		String flight_no = rs.getString("FlightNo");
+		        		o.put("airline_id", airline_id);
+		        		o.put("flightNumber", flight_no);
+		        		
+		        		
+		        		query = "{CALL getFlight(?,?)}";
+		        		stmt = conn.prepareCall(query);
+		        		stmt.setString(1, airline_id);
+		        		stmt.setString(2, flight_no);
+		        		ResultSet rs2 = stmt.executeQuery();
+		        		rs2.next();
+		        		o.put("daysOfWeek", rs2.getString("DaysOperating"));
+		        		
+	        			JSONObject legs = new JSONObject();
+		        		query = "{CALL getLeg(?,?)}";
+		        		stmt = conn.prepareCall(query);
+		        		stmt.setString(1, airline_id);
+		        		stmt.setString(2, flight_no);
+		        		rs2 = stmt.executeQuery();
+		        		while(rs2.next()) {
+		        			JSONObject legInfo = new JSONObject();
+		        			legInfo.put("depAirportID", rs2.getString("DepAirportID"));
+		        			legInfo.put("arrAirportID", rs2.getString("ArrAirportID"));
+		        			legInfo.put("depTime", rs2.getString("DepTime"));
+		        			legInfo.put("arrTime", rs2.getString("ArrTime"));
+		        			legs.put(rs2.getString("LegNo"), legInfo);
+		        		}
+		        		o.put("legs", legs);
+		        		
+		        		JSONObject fares = new JSONObject();
+		        		JSONObject fareInfo = new JSONObject();
+		        		String temp = null;
+		        		query = "{CALL getFare(?,?)}";
+		        		stmt = conn.prepareCall(query);
+		        		stmt.setString(1, airline_id);
+		        		stmt.setString(2, flight_no);
+		        		rs2 = stmt.executeQuery();
+		        		while(rs2.next()) {
+		        			String fare_type = rs2.getString("FareType");
+		        			if(temp != null && !temp.equals(fare_type)) {
+		        				fares.put(temp, fareInfo);
+		        				fareInfo = new JSONObject();
+		        			}
+		        			fareInfo.put(rs2.getString("Class"), rs2.getString("Fare"));
+		        			temp = fare_type;
+		        		}
+		        		if(temp != null) {
+		        			fares.put(temp, fareInfo);
+		        		}
+		        		o.put("price", fares);
+		        		jArray.add(o);
 		        }
+
+		        json.put("flights", jArray);
 				ConnectionUtils.close(conn);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -65,7 +112,6 @@ public class Popular extends HttpServlet {
 			e.printStackTrace();
 		}
 		
-        json.put("Popular", flights);
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
 		response.getWriter().print(json);
